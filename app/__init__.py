cfc9915b61311955d70c230e26d4d411dd6b3cd1
JuHong Jung <growingdever@gmail.com>
2016-11-12 19:05:13 +0900

    create routes directory to contain all route functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return render_template('index.html')
+from app.routes import *
