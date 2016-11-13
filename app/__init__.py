7d4916d855528470405f6ec206069e3ac397d28a
JuHong Jung <growingdever@gmail.com>
2016-11-13 18:45:29 +0900

    upload image to s3

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,17 @@ app.config.from_object('config')
 
 
 #
+# aws boto3
+#
+import boto3
+
+boto3_session = boto3.session.Session(
+	aws_access_key_id=app.config.get('AWS_ACCESS_KEY_ID'),
+	aws_secret_access_key=app.config.get('AWS_SECRET_ACCESS_KEY'),
+	region_name='ap-northeast-2')
+
+
+#
 # import model and create models
 #
 from flask_sqlalchemy import SQLAlchemy
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,5 @@
-from app import app
+import uuid
+from app import app, boto3_session
 from app.forms.image_upload import ImageUploadForm
 from flask import request, url_for, redirect, abort, render_template
 
@@ -16,5 +17,23 @@ def upload():
         abort(400)
 
     print(request.files['file'])
+
+    uploaded_file = request.files['file']
+
+    filename = uploaded_file.filename
+    hashed_filename = uuid.uuid4().hex[:16]
+    obj_key = 'images/{}'.format(hashed_filename)
+
+    s3 = boto3_session.resource('s3')
+    s3_bucket = s3.Bucket(app.config.get('AWS_S3_BUCKET_NAME'))
+    uploaded_obj = s3_bucket.put_object(
+        ACL='public-read',
+        Body=uploaded_file.stream.read(),
+        Key=obj_key,
+        ContentType=uploaded_file.mimetype,
+        Metadata={
+            'owner-content-type': 'document'
+        }
+    )
 
     return redirect(url_for('index'))
